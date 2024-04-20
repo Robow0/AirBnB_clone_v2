261d354f93b8d1d116c921e2c3ce9370f4f3d304
Robow0 <robowsalad@gmail.com>
2024-04-20 08:06:24 -0700

    updated

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -5,12 +5,19 @@
 from flask import Flask
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
-@app.route("/airbnb-onepage/", strict_slashes = False)
+@app.route("/")
 def home():
     """method that defines '/' route for Flask web application"""
     return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def hbnb():
+    """method that defines '/hbnb' route for Flask web application"""
+    return "HBNB"
 
 if __name__ == "__main__":
     app.run(host="localhost", port=5000)
