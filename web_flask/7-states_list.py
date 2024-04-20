79518c4e8799371cd9168ea11c61a1ab7a66fe15
Robow0 <robowsalad@gmail.com>
2024-04-20 09:31:19 -0700

    updated

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,27 +1,25 @@
 #!/usr/bin/python3
-
-
-"""utilizing Flask for Web app frame work"""
+"""
+starts a Flask web application
+"""
 
 from flask import Flask, render_template
-from models import storage, State
-from flask import escape
-
+from models import *
+from models import storage
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
-@app.route('/states_list')
-def show_states():
-    '''Dictionary: of States'''
-    state_dict = storage.all(State)
-    return render_template('7-states_list.html', state_dict=state_dict)
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """display a HTML page with the states listed in alphabetical order"""
+    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
+    return render_template('7-states_list.html', states=states)
 
 
 @app.teardown_appcontext
-def teardown_db(self):
-    '''Deletes the current session'''
+def teardown_db(exception):
+    """closes the storage on teardown"""
     storage.close()
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
